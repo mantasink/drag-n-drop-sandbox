7ebcc54bd02f02e455398f1c7cc9a945f790f67e
Mantas Sinkevicius <mantas.sinkevicius@teliacompany.com>
2025-03-04 12:28:01 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,63 @@
+import React, { useCallback, useState } from "react";
+import { useDropzone } from "react-dropzone";
+
+export default function App() {
+  const [files, setFiles] = useState<File[]>([]);
+  const [useFsAccessApi, setUseFsAccessApi] = useState<boolean>(false);
+
+  const onDrop = useCallback((acceptedFiles) => {
+    setFiles(acceptedFiles);
+  }, []);
+
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({
+    onDrop,
+    noClick: true,
+    useFsAccessApi: useFsAccessApi,
+  });
+
+  const handleCheckboxChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setUseFsAccessApi(event.target.checked);
+  };
+
+  return (
+    <div className="m-3">
+      <h1 className="text-3xl font-bold mb-1">Testing Drag'n'drop</h1>
+
+      <div className="mt-5">
+        <label className="flex items-center">
+          <input
+            type="checkbox"
+            checked={useFsAccessApi}
+            onChange={handleCheckboxChange}
+            className="mr-2"
+          />
+          Use File System Access API
+        </label>
+      </div>
+
+      <div {...getRootProps()}>
+        <input {...getInputProps()} />
+        <div
+          className={`h-80 rounded-lg flex items-center justify-center font-semibold ${
+            isDragActive ? "bg-green-500" : "bg-blue-700"
+          } text-white`}
+        >
+          {isDragActive
+            ? "Drop here"
+            : "Drag 'n' drop some files here, or click to select files"}
+        </div>
+      </div>
+
+      <div className="mt-5">
+        <h2 className="text-xl font-bold">Selected files:</h2>
+        <ul>
+          {files.map((file) => (
+            <li key={file.name} className="font-light">
+              {file.name}
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
